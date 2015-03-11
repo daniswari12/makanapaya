b65d18bb7928ddcc20ba8cb4c87e25a96612e672
daniswari12 <dansdyars@gmail.com>
2015-03-11 11:40:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Makan Apa Ya ?</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<center>
+<div id="header">
+  <div align="center"> <img src="images/b.png" width="118" height="80" border="0" align="absmiddle" />Makan Apa Ya ? 
+    <h6>Portal Informasi Pencarian Tempat Makan di Surabaya </h6>
+  </div>
+</div>
+<div id="navigasi">
+  <label for="username2"><br />
+  </label>
+  <input name="username" type="text" id="username2" value="username" />
+  <input name="password" type="password" id="password" value="password" />
+  <input type="submit" name="login" id="login" value="Sign in" />
+  <input type="submit" name="sign up" id="sign up" value="Sign up" />
+</div>
+<table width="900" border="0" cellspacing="5">
+  <tr>
+    <td width="57">Beranda</td>
+    <td width="97">Tentang Kami</td>
+    <td width="158">Daftar Tempat Makan</td>
+    <td width="271">Kontak</td>
+    <td width="222"><label for="search"></label>
+    <input name="search" type="text" id="search" value="search" /></td>
+  </tr>
+</table>
+<div align="left"></div>
+<div id="conten">
+  <table width="200" height="307" border="0" align="left">
+    <tr>
+      <th align="left" scope="row"><p align="justify"><strong>Cari Berdasarkan Kategori</strong></p>
+        <p align="justify">
+          <label for="harga">Harga</label>
+          <select name="harga" id="harga">
+          </select>
+        </p>
+        <p align="justify">
+          <label for="lokasi">Lokasi</label>
+          <select name="lokasi" id="lokasi">
+          </select>
+        </p>
+        <p align="justify">
+          <label for="jenismakanan">Jenis Makanan</label>
+          <select name="jenismakanan" id="jenismakanan">
+          </select>
+        </p>
+        <p align="justify">
+          <label for="jenistempatmakanan">Jenis Tempat Makanan</label>
+          <select name="jenistempatmakanan" id="jenistempatmakanan">
+          </select>
+        </p>
+        <p align="justify">
+          <label for="rating">Rating</label>
+          <select name="rating" id="rating">
+          </select>
+        </p></th>
+    </tr>
+</table>
+</div>
+<table width="900" border="0" cellpadding="8" cellspacing="10">
+  <tr>
+    <td width="254" bgcolor="#99CCCC">Tambah Makan Populer Bulan Ini</td>
+    <td colspan="2" bgcolor="#99CCCC">Tempat Makan Baru</td>
+  </tr>
+  <tr>
+    <td height="238" bgcolor="#D6D6D6">&nbsp;</td>
+    <td width="291" bgcolor="#D6D6D6">&nbsp;</td>
+    <td width="299" bgcolor="#D6D6D6">&nbsp;</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</center>
+</body>
+</html>
