8e8f264f1d7d190dc48957fde6c65e51ae760656
daniswari12 <dansdyars@gmail.com>
2015-03-30 19:16:07 +0700

    Update tentangkami.php

--- tentangkami.php
+++ tentangkami.php
@@ -16,57 +16,68 @@
 <div id="navigasi">
   <label for="username2"><br />
   </label>
-  <input name="username" type="text" id="username2" value="username" />
-  <input name="password" type="password" id="password" value="password" />
-  <input type="submit" name="login" id="login" value="Sign in" />
-  <input type="submit" name="sign up" id="sign up" value="Sign up" />
+  <table width="100%" border="0">
+    <tr>
+      <th width="100%" align="right" valign="middle" scope="row"><form action="actionloginvisitor.php" method="post">
+      <input name="username" type="text" id="username2" value="username" />
+        <input name="password" type="password" id="password" value="password" />
+        <input type="submit" name="login" id="login" value="Sign in" /></th>
+      <th width="67" align="right" valign="middle" scope="row"> <form action="signup.php">
+        <input name="signup" type="submit" id="signup" formaction="signup.php" value="Sign up" />
+      </form></th>
+    </tr>
+  </table>
 </div>
 <table width="900" border="0" cellspacing="5">
   <tr>
-    <td width="150">Tentang Kami</td>
-    <td width="168">&nbsp;</td>
-    <td width="289">&nbsp;</td>
+    <td width="57" style="font-size: 14px"><a href="index.php">Beranda</a></td>
+    <td width="97" style="font-size: 14px">Tentang Kami</td>
+    <td width="158" style="font-size: 14px"><a href="daftartempatmakan.php">Daftar Tempat Makan</a></td>
+    <td width="271" style="font-size: 14px"><a href="kontak.php">Kontak</a></td>
     <td width="239"><label for="search"></label>
-    <input name="search" type="text" id="search" value="search" /></td>
+      <input name="search" type="text" id="search" value="search" /></td>
   </tr>
 </table>
 <div align="left"></div>
 <div id="conten">
   <table width="200" height="307" border="0" align="left">
     <tr>
-      <th align="left" scope="row"><p align="justify"><strong>Cari Berdasarkan Kategori</strong></p>
-        <p align="justify">
-          <label for="harga">Harga</label>
+      <th align="left" scope="row"><strong style="font-family: Arial; font-size: 16px;">Cari Berdasarkan Kategori</strong></th>
+    </tr>
+    <tr>
+      <th align="left" scope="row"><p align="justify">
+        <label for="harga"><span style="font-family: Arial; font-size: 12px;">Harga</span></label>
+        <span style="font-family: Arial">
           <select name="harga" id="harga">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="lokasi">Lokasi</label>
           <select name="lokasi" id="lokasi">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="jenismakanan">Jenis Makanan</label>
           <select name="jenismakanan" id="jenismakanan">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="jenistempatmakanan">Jenis Tempat Makanan</label>
           <select name="jenistempatmakanan" id="jenistempatmakanan">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="rating">Rating</label>
           <select name="rating" id="rating">
           </select>
-        </p></th>
+        </span><span style="font-size: 12px"> </span></p></th>
     </tr>
-</table>
+    </table>
 </div>
 <table width="900" border="0" cellpadding="8" cellspacing="10">
   <tr>
-    <td width="254" bgcolor="#99CCCC">Tambah Makan Populer Bulan Ini</td>
-    <td colspan="2" bgcolor="#99CCCC">Tempat Makan Baru</td>
+    <td width="254" bgcolor="#99CCCC" style="font-family: Arial">Tambah Makan Populer Bulan Ini</td>
+    <td colspan="2" bgcolor="#99CCCC" style="font-family: Arial">Tempat Makan Baru</td>
   </tr>
   <tr>
     <td height="238" bgcolor="#D6D6D6">&nbsp;</td>
