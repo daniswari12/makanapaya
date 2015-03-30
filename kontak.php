78d0ac78ae43d1a4a40065d9e0376b940f7d2273
daniswari12 <dansdyars@gmail.com>
2015-03-30 19:16:45 +0700

    Update kontak.php

--- kontak.php
+++ kontak.php
@@ -21,50 +21,56 @@
   <input type="submit" name="login" id="login" value="Sign in" />
   <input type="submit" name="sign up" id="sign up" value="Sign up" />
 </div>
-<table width="512" border="0" cellspacing="5">
+<table width="900" border="0" cellspacing="5">
   <tr>
-    <td width="271">Kontak</td>
+    <td width="57" style="font-size: 14px">Beranda</td>
+    <td width="97" style="font-size: 14px">Tentang Kami</td>
+    <td width="158" style="font-size: 14px">Daftar Tempat Makan</td>
+    <td width="271" style="font-size: 14px">Kontak</td>
     <td width="222"><label for="search"></label>
-    <input name="search" type="text" id="search" value="search" /></td>
+      <input name="search" type="text" id="search" value="search" /></td>
   </tr>
 </table>
 <div align="left"></div>
 <div id="conten">
   <table width="200" height="307" border="0" align="left">
     <tr>
-      <th align="left" scope="row"><p align="justify"><strong>Cari Berdasarkan Kategori</strong></p>
-        <p align="justify">
-          <label for="harga">Harga</label>
+      <th align="left" scope="row"><strong style="font-family: Arial; font-size: 16px;">Cari Berdasarkan Kategori</strong></th>
+    </tr>
+    <tr>
+      <th align="left" scope="row"><p align="justify">
+        <label for="harga"><span style="font-family: Arial; font-size: 12px;">Harga</span></label>
+        <span style="font-family: Arial">
           <select name="harga" id="harga">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="lokasi">Lokasi</label>
           <select name="lokasi" id="lokasi">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="jenismakanan">Jenis Makanan</label>
           <select name="jenismakanan" id="jenismakanan">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="jenistempatmakanan">Jenis Tempat Makanan</label>
           <select name="jenistempatmakanan" id="jenistempatmakanan">
           </select>
-        </p>
-        <p align="justify">
+        </span></p>
+        <p align="justify"> <span style="font-family: Arial; font-size: 12px;">
           <label for="rating">Rating</label>
           <select name="rating" id="rating">
           </select>
-        </p></th>
+        </span><span style="font-size: 12px"> </span></p></th>
     </tr>
-</table>
+    </table>
 </div>
 <table width="900" border="0" cellpadding="8" cellspacing="10">
   <tr>
-    <td width="254" bgcolor="#99CCCC">Tambah Makan Populer Bulan Ini</td>
-    <td colspan="2" bgcolor="#99CCCC">Tempat Makan Baru</td>
+    <td width="254" bgcolor="#99CCCC" style="font-family: Arial">Tambah Makan Populer Bulan Ini</td>
+    <td colspan="2" bgcolor="#99CCCC" style="font-family: Arial">Tempat Makan Baru</td>
   </tr>
   <tr>
     <td height="238" bgcolor="#D6D6D6">&nbsp;</td>
