a0a8ff7378918e0cdf10a21b7f61ac33add2b029
daniswari12 <dansdyars@gmail.com>
2015-03-30 19:22:03 +0700

    Create notifikasi.php

---
+++ notifikasi.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Makan Apa Ya ?</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<center>
+<div id="header">
+  <div align="center"> <img src="images/b.png" width="118" height="80" border="0" align="absmiddle" />Makan Apa Ya ? 
+    <h6>Portal Informasi Pencarian Tempat Makan di Surabaya </h6>
+  </div>
+</div>
+<div id="navigasi">
+  <label for="username2"><br />
+  Halaman Backpanel Admin
+    <br />
+  </label>
+</div>
+<table width="900" border="0" cellspacing="5">
+  <tr>
+    <td width="80" style="font-size: 14px">Beranda</td>
+    <td width="100" style="font-size: 14px">Akun Klien</td>
+    <td width="100" style="font-size: 14px"><a href="admin_akun_user.php">Akun User</a></td>
+    <td width="128" style="font-size: 14px"><a href="admin_tempatmakan.php">Tempat Makan</a></td>
+    <td width="128" style="font-size: 14px">Testimoni</td>
+    <td width="222" style="font-size: 14px">Setting</td>
+  </tr>
+</table>
+<div align="left"></div>
+<div id="conten">
+  <table width="100%" height="30" border="0" align="center">
+    <tr>
+      <th width="800" height="26" align="center" bgcolor="#99CCCC" style="font-family: Arial; font-size: 14px;" scope="row">Notifikasi</th>
+      </tr>
+    </table>
+  <table width="100%" height="155">
+    <tr>
+      <th valign="bottom" scope="row"><input type="submit" name="logout" id="logout" value="Log Out" /></th>
+    </tr>
+  </table>
+</div>
+<table width="900" border="0" cellpadding="8" cellspacing="10">
+  <tr>
+    <td width="254" bgcolor="#99CCCC" style="font-family: Arial">Tambah Makan Populer Bulan Ini</td>
+    <td colspan="2" bgcolor="#99CCCC" style="font-family: Arial">Tempat Makan Baru</td>
+  </tr>
+  <tr>
+    <td height="238" bgcolor="#D6D6D6">&nbsp;</td>
+    <td width="291" bgcolor="#D6D6D6">&nbsp;</td>
+    <td width="299" bgcolor="#D6D6D6">&nbsp;</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</center>
+</body>
+</html>
