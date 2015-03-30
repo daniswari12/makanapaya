cd41227b16b23c3f3060fb19fabddc3efbf924aa
daniswari12 <dansdyars@gmail.com>
2015-03-30 19:23:24 +0700

    Create warninglogin.php

---
+++ warninglogin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Makan Apa Ya ?</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<center>
+<div id="header">
+  <div align="center"> <img src="images/b.png" width="118" height="80" border="0" align="absmiddle" />Makan Apa Ya ? 
+    <h6>Portal Informasi Pencarian Tempat Makan di Surabaya </h6>
+  </div>
+</div>
+<div id="navigasi">
+  <label for="username2"><br />
+  Halaman Backpanel Admin
+    <br />
+  </label>
+</div>
+<table width="900" border="0" cellspacing="5">
+  <tr>
+    <td width="57">&nbsp;</td>
+    <td width="97">&nbsp;</td>
+    <td width="158">&nbsp;</td>
+    <td width="271">&nbsp;</td>
+    <td width="222">&nbsp;</td>
+  </tr>
+</table>
+<div align="left"></div>
+<div id="conten">
+<form action="actionlogin.php" method="post">
+  <table width="100%" height="189" border="0" align="center">
+    <tr>
+      <th height="26" align="center" bgcolor="#99CCCC" style="font-family: Arial; font-size: 24px; color: #F00;" scope="row">WARNING !</th>
+      </tr>
+    <tr>
+      <th align="center" valign="middle" style="font-family: Arial; font-size: 16px;" scope="row"><p>Coba login dulu <a href="admin.php">klik link login</a></p></th>
+      </tr>
+    </table>
+  <table width="100%" border="0">
+    <tr>
+      <th scope="row">&nbsp;</th>
+    </tr>
+  </table></form>
+  
+</div>
+<p>&nbsp;</p>
+</center>
+</body>
+</html>
