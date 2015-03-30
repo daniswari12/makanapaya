b98d3b2e32c1695477b2f06a49c8d96bde3b324e
daniswari12 <dansdyars@gmail.com>
2015-03-30 19:19:01 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Makan Apa Ya ?</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<center>
+<div id="header">
+  <div align="center"> <img src="images/b.png" width="118" height="80" border="0" align="absmiddle" />Makan Apa Ya ? 
+    <h6>Portal Informasi Pencarian Tempat Makan di Surabaya </h6>
+  </div>
+</div>
+<div id="navigasi">
+  <label for="username2"><br />
+  Halaman Backpanel Admin
+    <br />
+  </label>
+</div>
+<table width="900" border="0" cellspacing="5">
+  <tr>
+    <td width="57">&nbsp;</td>
+    <td width="97">&nbsp;</td>
+    <td width="158">&nbsp;</td>
+    <td width="271">&nbsp;</td>
+    <td width="222">&nbsp;</td>
+  </tr>
+</table>
+<div align="left"></div>
+<div id="conten">
+<form action="actionlogin.php" method="post">
+  <table width="100%" height="189" border="0" align="center">
+    <tr>
+      <th height="26" colspan="2" align="center" bgcolor="#99CCCC" style="font-family: Arial; font-size: 16px;" scope="row">Admin Login</th>
+      </tr>
+    <tr>
+      <th width="179" align="left" valign="middle" style="font-family: Arial; font-size: 16px;" scope="row"><p>Username</p>
+        <p>Password</p></th>
+      <th width="671" align="left" valign="middle" scope="row"><p>
+        <label for="ausername">:</label>
+        <input name="ausername" type="text" required="required" id="ausername" value="username" />
+      </p>
+        <p>
+          <label for="apassword">:</label>
+          <input name="apassword" type="password" required="required" id="apassword" value="**********" />
+        </p></th>
+    </tr>
+    </table>
+  <table width="100%" border="0">
+    <tr>
+      <th scope="row"><input type="submit" name="login" id="login" value="Log in" /></th>
+    </tr>
+  </table></form>
+  
+</div>
+<table width="900" border="0" cellpadding="8" cellspacing="10">
+  <tr>
+    <td width="254" bgcolor="#99CCCC" style="font-family: Arial">Tambah Makan Populer Bulan Ini</td>
+    <td colspan="2" bgcolor="#99CCCC" style="font-family: Arial">Tempat Makan Baru</td>
+  </tr>
+  <tr>
+    <td height="238" bgcolor="#D6D6D6">&nbsp;</td>
+    <td width="291" bgcolor="#D6D6D6">&nbsp;</td>
+    <td width="299" bgcolor="#D6D6D6">&nbsp;</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</center>
+</body>
+</html>
